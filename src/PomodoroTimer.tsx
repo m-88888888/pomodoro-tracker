a5d1c28d70336fb1626b3945baf4f1bc0abf6707
m-88888888 <mochi.h8888@gmail.com>
2021-05-02 11:15:51 +0900

    :construction: WIP add pomodoro-timer

--- src/PomodoroTimer.tsx
+++ src/PomodoroTimer.tsx
@@ -1,37 +1,67 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useState, useEffect, useRef } from 'react';
+import { Typography, Button } from '@material-ui/core';
+import ReactHowler from 'react-howler';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+
+const padTime = (time: number) => {
+  return String(time).length === 1 ? `0${time}` : `${time}`;
+};
+const format = (time: number) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes}:${padTime(seconds)}`;
+};
 
 const PomodoroTimer: React.FC = () => {
-  const [time, setTime] = React.useState<number>(0);
-  const intervalRef = React.useRef<any>(null);
-  const start = () => {
-    if (intervalRef.current !== null) {
-      return;
+  // const [counter, setCounter] = useState(1500);
+  const [counter, setCounter] = useState(3);
+  const [timerId, setTimerId] = useState<NodeJS.Timeout | number>(0);
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    if (counter === 0) {
+      clearInterval(timerId as number);
+      setPlaying((prevState) => !prevState);
     }
-    intervalRef.current = setInterval(() => {
-      setTime((t) => t + 1);
-    }, 1000);
+  }, [counter, timerId]);
+
+  const start = () => {
+    setTimerId(
+      setInterval(() => {
+        setCounter((c) => c - 1);
+      }, 1000)
+    );
   };
   const stop = () => {
-    if (intervalRef.current === null) {
-      return;
-    }
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
+    clearInterval(timerId as number);
   };
-  const reset = () => setTime(0);
+  const reset = () => {
+    // setCounter(1500);
+    setCounter(3);
+    clearInterval(timerId as number);
+  };
+
   return (
     <>
-      <h1>{time}</h1>
+      <Typography variant="h1">{format(counter)}</Typography>
       <Button onClick={start} variant="contained" color="primary">
         Start
       </Button>
       <Button onClick={stop} variant="contained" color="secondary">
         Stop
       </Button>
-      <Button onClick={reset} variant="contained">
-        Reset
-      </Button>
+      <Button onClick={reset}>Reset</Button>
+      <ReactHowler
+        src="http://www.kurage-kosho.info/mp3/button83.mp3"
+        playing={playing}
+        volume={0.5}
+      />
+      {playing ? (
+        <PauseCircleOutlineIcon onClick={() => setPlaying((state) => !state)} />
+      ) : (
+        <PlayCircleOutlineIcon onClick={() => setPlaying((state) => !state)} />
+      )}
     </>
   );
 };
