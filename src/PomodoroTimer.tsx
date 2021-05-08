7c1b4dd736b63242e21ea8ceb21ef718251c29a5
m-88888888 <mochi.h8888@gmail.com>
2021-05-09 00:04:15 +0900

    :sparkles: Add pomodoro timer comonent

--- src/PomodoroTimer.tsx
+++ src/PomodoroTimer.tsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Typography, Button } from '@material-ui/core';
-import ReactHowler from 'react-howler';
-import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import {
+  Typography,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  makeStyles,
+  Container,
+} from '@material-ui/core';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import StatusMessage from './StatusMessage';
+
+const WORKING_TIME = 1500;
+const BREAK_TIME = 300;
+const color = lightBlue[500];
+const useStyles = makeStyles((theme) => ({
+  card: {
+    backgroundColor: color,
+  },
+}));
 
 const padTime = (time: number) => {
   return String(time).length === 1 ? `0${time}` : `${time}`;
@@ -13,18 +29,28 @@ const format = (time: number) => {
   return `${minutes}:${padTime(seconds)}`;
 };
 
+const audio = new Audio('http://www.kurage-kosho.info/mp3/button83.mp3');
+
 const PomodoroTimer: React.FC = () => {
-  // const [counter, setCounter] = useState(1500);
-  const [counter, setCounter] = useState(3);
+  const classes = useStyles();
+  const [counter, setCounter] = useState<number>(WORKING_TIME);
   const [timerId, setTimerId] = useState<NodeJS.Timeout | number>(0);
-  const [playing, setPlaying] = useState(false);
+  const [session, setSession] = useState<boolean>(false); // ボタン管理用
+  const [working, setWorking] = useState<boolean>(true); // 作業中
 
   useEffect(() => {
     if (counter === 0) {
       clearInterval(timerId as number);
-      setPlaying((prevState) => !prevState);
+      audio.play();
+      working ? setCounter(BREAK_TIME) : setCounter(WORKING_TIME);
+      setTimerId(
+        setInterval(() => {
+          setCounter((c) => c - 1);
+        }, 1000)
+      );
+      setWorking((w) => !w);
     }
-  }, [counter, timerId]);
+  }, [counter, timerId, working]);
 
   const start = () => {
     setTimerId(
@@ -32,36 +58,56 @@ const PomodoroTimer: React.FC = () => {
         setCounter((c) => c - 1);
       }, 1000)
     );
+    setSession(true);
   };
   const stop = () => {
     clearInterval(timerId as number);
+    setSession(false);
   };
   const reset = () => {
-    // setCounter(1500);
-    setCounter(3);
     clearInterval(timerId as number);
+    setCounter(WORKING_TIME);
+    setSession(false);
+    setWorking(true);
   };
 
   return (
     <>
-      <Typography variant="h1">{format(counter)}</Typography>
-      <Button onClick={start} variant="contained" color="primary">
-        Start
-      </Button>
-      <Button onClick={stop} variant="contained" color="secondary">
-        Stop
-      </Button>
-      <Button onClick={reset}>Reset</Button>
-      <ReactHowler
-        src="http://www.kurage-kosho.info/mp3/button83.mp3"
-        playing={playing}
-        volume={0.5}
-      />
-      {playing ? (
-        <PauseCircleOutlineIcon onClick={() => setPlaying((state) => !state)} />
-      ) : (
-        <PlayCircleOutlineIcon onClick={() => setPlaying((state) => !state)} />
-      )}
+      <Container maxWidth="sm">
+        <Card className={classes.card}>
+          <CardContent>
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              justify="center"
+              spacing={2}>
+              <Grid item xs={12}>
+                <StatusMessage working={working} session={session} />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h1">{format(counter)}</Typography>
+              </Grid>
+            </Grid>
+            <Grid container alignItems="center" justify="center" spacing={1}>
+              <Grid item>
+                {session ? (
+                  <Button onClick={stop} variant="contained" color="secondary">
+                    Stop
+                  </Button>
+                ) : (
+                  <Button onClick={start} variant="contained" color="primary">
+                    Start
+                  </Button>
+                )}
+              </Grid>
+              <Grid item>
+                <Button onClick={reset}>Reset</Button>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   );
 };
